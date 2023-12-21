611c2482ddcd1c615b55208148df160e3691ef41
sauki futaki <utaki.sauki19@gmail.com>
2023-12-21 23:43:19 +0700

    axios error

--- src/app/populer/page.tsx
+++ src/app/populer/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+
 import Head from "../header";
 import Pagination from "@/components/utils/Pagination";
 import AnimeList from "@/components/animelist";
